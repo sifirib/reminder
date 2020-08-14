d0154ea4d741b71df97ff2580583aed01621cdbf
hsrv <ahusrevceker@gmail.com>
2020-08-14 04:03:25 +0300

    Features&Requires&Missing&HaveToDo
    ~For now, you must set notifications you want on a text file called 'tasks'. Example:  meeting time-13:45
                                                                                                                                      live broadcast-22:30
                                                                                                                                      call my brother-17:21
    
    ~I'm going to add GUI to take input about notifications. You will not have to edit or add notification on a text file.
    ~You have to change the path in reminder.py with your that where you want to place the reminder file.
    ~It requires schedule library on python.
    ~You can run it at startup as a service on linux.

---
+++ reminder/reminder.py
@@ -0,0 +1,81 @@
+import os
+import schedule
+from datetime import datetime
+import sys
+# import psutil
+
+path = "/home/hrx/Desktop/reminder/tasks"
+
+os.system(f'notify-send "Welcome to Hawaii"')
+
+# def checkIfProcessRunning(processName):
+  
+#     for proc in psutil.process_iter():
+#         try:
+#             if processName.lower() in proc.name().lower():
+#                 return True
+#         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+#             pass
+#     return False
+
+
+read_file = open(rf"{path}","r")
+tasks = []
+
+
+for task in read_file.readlines():
+    data_task, data_date = task.split("-")
+    tasks.append([data_task, data_date[:-1]])
+    
+
+print(tasks)
+read_file.close()
+
+
+tasknumber = 0        
+
+while True:
+    
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    nearest_time = 10000
+
+    for i in range(0,len(tasks)):
+        aux_hour,aux_minute = tasks[i][1].split(":")
+        aux_time = int(aux_hour)*60 + int(aux_minute)
+        
+        current_hour,current_minute = current_time.split(":")
+        int_current_time = int(current_hour)*60 + int(current_minute)
+        
+        if  ((aux_time - int_current_time) < nearest_time) and (aux_time > int_current_time + 0.5):
+            print(i)
+            nearest_time = aux_time- int_current_time
+            k = i
+            
+        if (i == len(tasks) - 1):
+            time_for_schedule = tasks[k][1]
+            text_for_not = tasks[k][0]
+            print(text_for_not)
+            break
+   
+    def do_notification():
+        os.system(f'notify-send "{text_for_not}"')
+    
+    
+    schedule.every().day.at(f"{time_for_schedule}").do(do_notification)
+    
+    tasknumber += 1
+
+    while 1:
+        schedule.run_pending()
+        
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        if current_time == f"{time_for_schedule}:05":
+            # print("yes")
+            if tasknumber == len(tasks):
+                sys.exit("All tasks were notificationed, bye for today <:")
+            break
+        
+        # else:
+             # print("no")
